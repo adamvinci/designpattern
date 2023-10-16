d9317ed7b0bbb098609e8e685b89a542aedd5642
Adame Barhdadi <adame.barhdadi@student.vinci.be>
2023-10-16 12:18:29 +0200

    enum

--- seance4/src/MachineACafe.java
+++ seance4/src/MachineACafe.java
@@ -1,11 +1,80 @@
 public class MachineACafe {
+	public enum State{
+		PAS_ASSEZ{
+			@Override
+			public void selectionneBoisson(MachineACafe machineACafe,ToucheBoisson toucheBoisson) {
+				if(machineACafe.getBoisson()!=null && machineACafe.getBoisson() !=toucheBoisson ){
+					throw new IllegalStateException();
+				}
+			}
 
+			@Override
+			public void entrerMonnaie(MachineACafe machineACafe, Piece piece) {
+				if(machineACafe.getBoisson().getPrix()<= machineACafe.getMontantEnCours()){
+
+					machineACafe.setMontantEnCours(machineACafe.getMontantEnCours()-machineACafe.getBoisson().getPrix());
+					machineACafe.afficherBoisson(machineACafe.getBoisson());
+					machineACafe.setBoisson(null);
+					machineACafe.afficherMontant();
+					if (machineACafe.getMontantEnCours() == 0)
+						machineACafe.setState(IDLE);
+					else
+						machineACafe.setState(COLLECTE);
+				}else{
+					machineACafe.afficherPasAssez(machineACafe.getBoisson());
+
+				}
+
+
+
+
+			}
+		},COLLECTE{
+			@Override
+			public void selectionneBoisson(MachineACafe machineACafe ,ToucheBoisson toucheBoisson) {
+				if (toucheBoisson.getPrix() > machineACafe.getMontantEnCours()) {
+					machineACafe.setBoisson(toucheBoisson);
+					machineACafe.afficherPasAssez(toucheBoisson);
+					machineACafe.setState(PAS_ASSEZ);
+					return;
+				}
+				machineACafe.setMontantEnCours(machineACafe.getMontantEnCours()-toucheBoisson.getPrix());
+				machineACafe.afficherBoisson(toucheBoisson);
+				machineACafe.afficherMontant();
+				machineACafe.setBoisson(null);
+				if (machineACafe.getMontantEnCours() == 0)
+					machineACafe.setState(IDLE);
+			}
+		},IDLE{
+			@Override
+		public void selectionneBoisson(MachineACafe machineACafe, ToucheBoisson toucheBoisson) {
+			machineACafe.afficherPasAssez(toucheBoisson);
+		}
+
+			public void rendreMonnaie(MachineACafe machineACafe){
+
+
+			}};
+		public abstract void selectionneBoisson(MachineACafe machineACafe, ToucheBoisson toucheBoisson);
+
+		public void entrerMonnaie(MachineACafe machineACafe, Piece piece) {
+			machineACafe.setState(COLLECTE);
+
+		}
+
+		public void rendreMonnaie(MachineACafe machineACafe) {
+			machineACafe.afficherRetour();
+			machineACafe.setBoisson(null);
+			machineACafe.setMontantEnCours(0);
+			machineACafe.setState(IDLE);
+		}
+	}
 	private int montantEnCours = 0;
 	private State etatCourant ;
 	private ToucheBoisson boisson = null;
 
 	public MachineACafe() {
-		setState(new Idle());
+		setState(State.COLLECTE);
 	}
 
 	
